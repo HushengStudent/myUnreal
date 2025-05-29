b3067db5d844b3ee84565e93d9fb8f4fdd13d07d
HushengStudent <husheng.student@foxmail.com>
2025-05-29 23:27:33 +0800

    update

---
+++ Project/Source/Project/Framework/Core/ProjectBaseGameStateBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "ProjectBaseGameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API AProjectBaseGameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Project/Source/Project/Framework/Library/ProjectGameplayStatics.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/GameplayStatics.h"
+#include "ProjectGameplayStatics.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API UProjectGameplayStatics : public UGameplayStatics
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
