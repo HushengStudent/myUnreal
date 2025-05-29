b0467928ce4c0845c19e31c25151647b0819ee41
HushengStudent <husheng.student@foxmail.com>
2025-05-29 23:32:41 +0800

    update

--- Project/Source/Project/Project.h
+++ Project/Source/Project/Project.h
@@ -3,3 +3,21 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+#define IS_CLIENT (NM_Client == GetNetMode())
+
+#define IS_SERVER (NM_DedicatedServer == GetNetMode() || NM_ListenServer == GetNetMode())
+
+
+#define CHECK_CONDITION(Condition)\
+	if (false == (Condition))\
+	{\
+		return;\
+	}
+
+#define CHECK_CONDITION_WITH_MSG(Condition, LogCategory, LogType, Msg, ...)\
+	if (false == (Condition))\
+	{\
+		UE_LOG(LogCategory, LogType, Msg, ##__VA_ARGS__);\
+		return;\
+	}
