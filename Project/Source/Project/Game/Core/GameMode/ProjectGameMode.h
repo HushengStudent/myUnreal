c692e33288c345939048b76c08fccec6477262f8
HushengStudent <husheng.student@foxmail.com>
2025-06-08 22:04:39 +0800

    update

---
+++ Project/Source/Project/Game/Core/GameMode/ProjectGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Project/Framework/Core/ProjectBaseGameMode.h"
+#include "ProjectGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API AProjectGameMode : public AProjectBaseGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Project/Source/Project/Game/Core/PlayerState/ProjectPlayerState.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Project/Framework/Core/ProjectBasePlayerState.h"
+#include "ProjectPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API AProjectPlayerState : public AProjectBasePlayerState
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
