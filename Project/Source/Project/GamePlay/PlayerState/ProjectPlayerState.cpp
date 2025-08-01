7652c6fd0606dc27bcffa16b7ee15aff3b7dda4f
HushengStudent <husheng.student@foxmail.com>
2025-08-01 23:52:33 +0800

    update

--- Project/Source/Project/GamePlay/PlayerState/ProjectPlayerState.cpp
+++ Project/Source/Project/GamePlay/PlayerState/ProjectPlayerState.cpp
@@ -7,15 +7,64 @@
 #include "Project/SubSystem/World/ProjectDelegateWorldSubsystem.h"
 
 
-void AProjectPlayerState::BeginPlay()
-{
-	Super::BeginPlay();
-	if(AProjectGameStateBase* GameStateBase = UProjectGameplayStatics::GetProjectGameState(GetWorld()))
-	{
+// void AProjectPlayerState::BeginPlay()
+// {
+// 	Super::BeginPlay();
+// 	if(AProjectGameStateBase* GameStateBase = UProjectGameplayStatics::GetProjectGameState(GetWorld()))
+// 	{
+//
+// 	}
+// 	if(const UProjectDelegateWorldSubsystem* DelegateWorldSubsystem = GetWorld()->GetSubsystem<UProjectDelegateWorldSubsystem>(); DelegateWorldSubsystem && DelegateWorldSubsystem->OnPlayerStateAdd.IsBound())
+// 	{
+// 		DelegateWorldSubsystem->OnPlayerStateAdd.Broadcast(this);
+// 	}
+// }
 
-	}
-	if(const UProjectDelegateWorldSubsystem* DelegateWorldSubsystem = GetWorld()->GetSubsystem<UProjectDelegateWorldSubsystem>(); DelegateWorldSubsystem && DelegateWorldSubsystem->OnPlayerStateAdd.IsBound())
-	{
-		DelegateWorldSubsystem->OnPlayerStateAdd.Broadcast(this);
-	}
+AProjectPlayerState::AProjectPlayerState(const FObjectInitializer& ObjectInitializer)
+{
+}
+
+AProjectPlayerController* AProjectPlayerState::GetProjectPlayerController() const
+{
+	return Cast<AProjectPlayerController>(GetOwner());
+}
+
+UAbilitySystemComponent* AProjectPlayerState::GetAbilitySystemComponent() const
+{
+	return GetProjectAbilitySystemComponent();
+}
+
+void AProjectPlayerState::PreInitializeComponents()
+{
+	Super::PreInitializeComponents();
+}
+
+void AProjectPlayerState::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+}
+
+void AProjectPlayerState::Reset()
+{
+	Super::Reset();
+}
+
+void AProjectPlayerState::ClientInitialize(AController* C)
+{
+	Super::ClientInitialize(C);
+}
+
+void AProjectPlayerState::CopyProperties(APlayerState* PlayerState)
+{
+	Super::CopyProperties(PlayerState);
+}
+
+void AProjectPlayerState::OnDeactivated()
+{
+	Super::OnDeactivated();
+}
+
+void AProjectPlayerState::OnReactivated()
+{
+	Super::OnReactivated();
 }
--- Project/Source/Project/GamePlay/PlayerState/ProjectPlayerState.h
+++ Project/Source/Project/GamePlay/PlayerState/ProjectPlayerState.h
@@ -3,18 +3,44 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
 #include "ModularPlayerState.h"
+#include "Project/GamePlay/AbilitySystem/ProjectAbilitySystemComponent.h"
 #include "ProjectPlayerState.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class PROJECT_API AProjectPlayerState : public AModularPlayerState
+class PROJECT_API AProjectPlayerState : public AModularPlayerState, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
-	
-	
-	virtual void BeginPlay() override;
-	
+
+public:
+	AProjectPlayerState(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	UFUNCTION(BlueprintCallable, Category = "GamePlay|PlayerState")
+	AProjectPlayerController* GetProjectPlayerController() const;
+
+	UFUNCTION(BlueprintCallable, Category = "GamePlay|PlayerState")
+	UProjectAbilitySystemComponent* GetProjectAbilitySystemComponent() const { return AbilitySystemComponent; }
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	//~AActor interface
+	virtual void PreInitializeComponents() override;
+	virtual void PostInitializeComponents() override;
+	//~End of AActor interface
+
+	//~APlayerState interface
+	virtual void Reset() override;
+	virtual void ClientInitialize(AController* C) override;
+	virtual void CopyProperties(APlayerState* PlayerState) override;
+	virtual void OnDeactivated() override;
+	virtual void OnReactivated() override;
+	//~End of APlayerState interface
+
+	private:
+	// The ability system component subobject for game-wide things (primarily gameplay cues)
+	UPROPERTY(VisibleAnywhere, Category = "GamePlay|GameState")
+	TObjectPtr<UProjectAbilitySystemComponent> AbilitySystemComponent;
 };
